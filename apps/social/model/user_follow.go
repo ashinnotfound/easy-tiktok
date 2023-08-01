cc528a3f9d0392aa2fb14d231bd4a90a6d56b354
lql <1964757607@qq.com>
2023-08-01 21:34:46 +0800

    feat(model):定义user_follow中间表和消息model

---
+++ apps/social/model/user_follow.go
@@ -0,0 +1,10 @@
+package model
+
+import orm "easy-tiktok/db/mysql"
+
+type UserFollow struct {
+	orm.Model
+	UserId   int64 `gorm:"column:user_id;NOT NULL;comment:'用户id'"`
+	FollowId int64 `gorm:"column:follow_id;NOT NULL;comment:'关注用户id'"`
+	Status   int32 `gorm:"column:status;NOT NULL;comment:'关注状态'"`
+}
