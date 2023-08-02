ec07d36d00d380442417a8f89de9590f023f2d0f
lql <1964757607@qq.com>
2023-08-02 22:41:48 +0800

    feat(service):完善relation服务中的Action方法

--- apps/app/internal/service/social_relation.go
+++ apps/app/internal/service/social_relation.go
@@ -1,6 +1,13 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"easy-tiktok/apps/app/internal/rpc"
+	pb "easy-tiktok/apps/social/proto"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 // RelationService //
 // 关系方面的服务
@@ -11,7 +18,23 @@ type RelationService struct {
 // Action //
 // 关注or取消关注
 func (service *RelationService) Action(c *gin.Context) {
-
+	token := c.Query("token")
+	toUserId, _ := strconv.ParseInt(c.Query("to_user_id"), 10, 64)
+	actionTypeInt64, _ := strconv.ParseInt(c.Query("action_type"), 10, 32)
+	actionType := int32(actionTypeInt64)
+	socialClient := rpc.GetSocialRpc()
+	response, err := socialClient.RelationAction(context.Background(),
+		&pb.DouyinRelationActionRequest{
+			Token:      &token,
+			ToUserId:   &toUserId,
+			ActionType: &actionType,
+		})
+	if err != nil {
+		//global.LOGGER.Errorf("RelationService::Action方法出错,reason: %v\n", err)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 // FollowList //
