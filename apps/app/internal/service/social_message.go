97d3de2186afdf767f388769e787c2172cfd1adf
lql <1964757607@qq.com>
2023-08-02 19:53:20 +0800

    feat(service):初始化服务

---
+++ apps/app/internal/service/social_message.go
@@ -0,0 +1,21 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+// MessageService //
+// 消息方面的服务
+// Author lql
+type MessageService struct {
+}
+
+// Action //
+// 发消息
+func (service *MessageService) Action(c *gin.Context) {
+
+}
+
+// Chat //
+// 聊天
+func (service *MessageService) Chat(c *gin.Context) {
+
+}
---
+++ apps/app/internal/service/social_relation.go
@@ -0,0 +1,33 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+// RelationService //
+// 关系方面的服务
+// Author lql
+type RelationService struct {
+}
+
+// Action //
+// 关注or取消关注
+func (service *RelationService) Action(c *gin.Context) {
+
+}
+
+// FollowList //
+// 获取关注列表
+func (service *RelationService) FollowList(c *gin.Context) {
+
+}
+
+// FollowerList //
+// 获取粉丝列表
+func (service *RelationService) FollowerList(c *gin.Context) {
+
+}
+
+// FriendList //
+// 获取好友列表
+func (service *RelationService) FriendList(c *gin.Context) {
+
+}
