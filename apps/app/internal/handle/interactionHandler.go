054c0333ec74481875eb6415d0f3440cc31c8da9
ashinnotfound <ashinnotfound@qq.com>
2023-08-02 19:21:28 +0800

    feat(handle):新增getFavoriteListHandler

--- apps/app/internal/handle/interactionHandler.go
+++ apps/app/internal/handle/interactionHandler.go
@@ -26,7 +26,20 @@ func favoriteHandler(context *gin.Context) {
 }
 
 func getFavoriteListHandler(context *gin.Context) {
+	token := context.Query("token")
+	userId, _ := strconv.ParseInt(context.Query("user_id"), 10, 64)
 
+	req := interaction.DouyinFavoriteListRequest{
+		Token:  &token,
+		UserId: &userId,
+	}
+
+	favoriteList, err := rpc.GetInteractionRpc().GetFavoriteList(context, &req)
+	if err != nil {
+		context.JSON(400, favoriteList)
+		return
+	}
+	context.JSON(200, favoriteList)
 }
 
 func commentHandler(context *gin.Context) {
