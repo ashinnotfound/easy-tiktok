af479d279e1d6888bb8b365942e14f2365499bc2
lql <1964757607@qq.com>
2023-08-02 14:47:50 +0800

    feat(logic):完善relation服务中取消关注和关注用户的Action接口

--- apps/social/internal/logic/relation_logic.go
+++ apps/social/internal/logic/relation_logic.go
@@ -2,7 +2,12 @@ package logic
 
 import (
 	"context"
+	"easy-tiktok/apps/constant"
+	"easy-tiktok/apps/global"
+	"easy-tiktok/apps/social/model"
 	pb "easy-tiktok/apps/social/proto"
+	jwt "easy-tiktok/util"
+	"errors"
 )
 
 // RelationServiceImpl //
@@ -15,8 +20,41 @@ type RelationServiceImpl struct {
 // Action //
 // 取消关注和关注用户
 func (impl *RelationServiceImpl) Action(ctx context.Context, request *pb.DouyinRelationActionRequest) (*pb.DouyinRelationActionResponse, error) {
+	// 通过token获取用户id
+	userId := jwt.GetUserId(request.GetToken())
 
-	return nil, nil
+	// 获取中间表
+	userFollowTable := global.DB.Table(model.USER_FOLLOW_TABLE)
+	// 查询中间表
+	var po model.UserFollow
+	// 初始化响应
+	var response *pb.DouyinRelationActionResponse
+	*response.StatusCode = constant.STATUS_OK
+
+	// 逻辑判断
+	if result := userFollowTable.Where(&model.UserFollow{UserId: userId, FollowId: request.GetToUserId()}).First(&po); result.Error != nil {
+		// 查询数据库出现问题
+		*response.StatusCode = constant.STATUS_FAILED
+		*response.StatusMsg = "数据库层面出现问题,Action接口调用失败"
+		return response, result.Error
+	} else if result.RowsAffected == 0 {
+		// 数据库中没有记录
+		if request.GetActionType() == 1 {
+			po = model.UserFollow{UserId: userId, FollowId: request.GetToUserId(), Status: request.GetActionType()}
+			userFollowTable.Create(&po)
+		} else if request.GetActionType() == 2 {
+			*response.StatusCode = constant.STATUS_FAILED
+			*response.StatusMsg = "无效的输入参数,请注意前端代码"
+			return response, errors.New(*response.StatusMsg)
+		}
+	} else {
+		if po.Status != request.GetActionType() {
+			po.Status = request.GetActionType()
+			userFollowTable.Model(&po).Update("status", po.Status)
+		}
+	}
+	*response.StatusMsg = "Action操作成功"
+	return response, nil
 }
 
 // GetFollowList //
